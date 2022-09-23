302da4ca55707b05514f6b1dfb0468b59a4809c3
DaniVillamar <villamar959@gmail.com>
2022-09-22 22:56:35 -0500

    Entregable1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConocemeComponent } from './componets/conoceme/conoceme.component';
+import { RedesComponent } from './componets/redes/redes.component';
+import { ConocimientosComponent } from './componets/conocimientos/conocimientos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConocemeComponent,
+    RedesComponent,
+    ConocimientosComponent
   ],
   imports: [
     BrowserModule
